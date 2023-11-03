a7c71a1f91e8190484cff7ac42a82a15127b082b
micheal <michealagunbiade1@gmail.com>
2023-11-02 17:20:13 -0700

    edited

--- components/body.tsx
+++ components/body.tsx
@@ -19,12 +19,12 @@ export default function Body(){
         <div className="relative   top-20 left-[150px]  w-[180px]  text-slate-300 text-[8px]"> Feel Free to talk to us about anything Creativity, We will always be here to help you find that solution</div>
         <div className="absolute   left-[900px] w-[200px] top-10 text-white font-bold text-[15px]">You cant use up Creativity, the more you use, More you have in your siginificant mind.</div>
         <div className="absolute  top-[165px] right-[230px] w-[180px]  text-slate-300 text-[8px]"> What are you waiting for, Dont wait to be Motivated, Hit me up lets do something Beautifu, Tiome Waits for no One</div>
-        <a  className="relative top-[150px]  text-slate-400 text-[15px] left-[150px]  text-yellow-400" href="Michealagunbiade1@gmail.com"> Micheal Agunbiade --> </a>
-        <div  className=" relative  left-[900px] top-[120px]  text-slate-400 text-[30px]  font-bold text-yellow-400"> 14 </div>
-        <div  className=" relative top-[82px] left-[950px]  text-slate-400 text-[10px] w-14 text-white"> Years Of Experience</div>
-        <div  className=" relative  left-[1080px] top-[45px]  text-slate-400 text-[30px]  font-bold text-yellow-400"> 120 </div>
-        <div  className=" relative top-[05px] left-[1150px]  text-slate-400 text-[10px] w-14 text-white"> Job Accomplished</div>
-        <div className=" bg-slate-700 h-[250px] relative top-[90px]"> </div>
+        <a  className="relative top-[150px]  text-[15px] left-[150px]  text-yellow-400" href="Michealagunbiade1@gmail.com"> Micheal Agunbiade -- </a>
+        <div  className=" relative  left-[900px] top-[120px] text-[30px]  font-bold text-yellow-400"> 14 </div>
+        <div  className=" relative top-[82px] left-[950px]  text-[10px] w-14 text-white"> Years Of Experience</div>
+        <div  className=" relative  left-[1080px] top-[45px] text-[30px]  font-bold text-yellow-400"> 120 </div>
+        <div  className=" relative top-[05px] left-[1150px]  text-[10px] w-14 text-white"> Job Accomplished</div>
+        <div className=" bg-slate-700 h-[1000px] relative top-[90px]"> </div>
         <div className=" bg-yellow-400 h-[220px] w-[220px] absolute top-[350px] left-[300px]"> 
         <div className=" w-12  top-[110px]  left-[100px] text-[20px] text-white relative">Product Designer</div>
         <div className="   top-[120px]  left-[100px] text-[10px] text-white relative">2 Project</div>
@@ -38,8 +38,26 @@ export default function Body(){
         <div className="   top-[120px]  left-[100px] text-[10px] text-white relative">2 Project</div>
         </div>
 
-
-
+        <div className="absolute  top-[680px]  right-[1110px]  text-slate-400 text-[10px] "> -Portfolio  </div>
+        <div className="absolute  top-[700px]  right-[905px] w-[250px] text-white font-bold   text-[20px]"> All Creative Work Selected Job </div>
+        <div className="absolute  top-[770px] right-[975px]  w-[180px]  text-slate-300 text-[8px]"> Creativity come from thinking outside the Box, Going more than your Imagination</div>
+        <a  className="absolute  top-[960px] right-[1050px]   text-[15px]    text-yellow-400" href="Michealagunbiade1@gmail.com"> Explore More-- </a>
+        <div className="  w-[460px] h-3 top-[650px]  left-[710px] absolute"> <img src="chu.jpg" alt="logo"></img> </div>
+        <div className="  w-[460px] h-3 top-[1100px]  left-[210px] absolute"> <img src="but.webp" alt="logo"></img> </div>
+        <div className="  w-[360px] h-3 top-[1000px]  left-[710px] absolute"> <img src="buy.jpg" alt="logo"></img> </div>
+        <div className="  bg-slate-800 h-[1150px]  top-[100px]"> 
+        <div className=" absolute  top-[1480px]  right-[1130px]  text-slate-400 text-[10px] "> - Blog  </div>
+        <div className="absolute  top-[1500px]  right-[990px] w-[170px] text-white font-bold   text-[20px]"> Whats new?  </div>
+        <div className="absolute  top-[1530px]  right-[970px] w-[190px] text-white font-bold   text-[20px]"> My blog and news </div>
+        <div className="relative top-[200px] left-[650px] text-slate-400 text-[15px]"> Feb 26 - Blog   </div>
+        <div className="relative  left-[770px]  top-[173px] text-white font-bold text-[20px]"> Let this be a lesson to you </div>
+        <div className="relative top-[200px] left-[650px] text-slate-400 text-[15px]"> Feb 24 - Blog   </div>
+        <div className="relative  left-[770px]  top-[173px] text-white font-bold text-[20px]"> How do you use your Time Tracking for Projects </div>
+        <div className="relative top-[200px] left-[650px] text-slate-400 text-[15px]"> Feb 9 - Blog   </div>
+        <div className="relative  left-[770px]  top-[173px] text-white font-bold text-[20px]"> Ego and Empathy in Design </div>
+        <div className="  w-[290px] h-3 top-[400px]  left-[110px] relative"> <img src="pro.jpeg" alt="logo"></img> </div>
+        <div className="relative  left-[700px]  w-80 top-[500px] text-white  font-extrabold text-[30px]"> ALLOW YOURSELF TO BE A BEGINNER. NO ONE START OF BEING EXCELLENT</div>
+        </div>
         </div>
         </div>
     )
